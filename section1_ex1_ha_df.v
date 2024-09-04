27808e1069798236546f2a26992b1a73f9726f80
wwwwwe18 <60565177+wwwwwe18@users.noreply.github.com>
2024-09-04 10:22:06 +0100

    Add files via upload

---
+++ section1_ex1_ha_df.v
@@ -0,0 +1,17 @@
+//*****************************************************
+// Project		: Half adder - dataflow
+// File			: section1_ex1_ha_df
+// Editor		: Wenmei Wang
+// Date			: 04/09/2024
+// Description	: Half adder - dataflow
+//*****************************************************
+
+module HA(s,c,a,b);
+
+input	a,b;
+output	s,c;
+
+assign	s = a^b;
+assign	c = a&b;
+
+endmodule
